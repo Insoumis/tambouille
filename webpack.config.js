328796e6ce00da1c459ef6522a02f35c0002b477
Chloé Gence <chloe.gence@gmail.com>
2017-05-20 16:27:13 +0200

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const webpack = require('webpack')
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: [
     'babel-polyfill',
     'react-hot-loader/patch',
-    './src/index'
+    './src/index',
   ],
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
   },
   plugins: [
-    new webpack.NamedModulesPlugin()
-  ]
-}
+    new webpack.NamedModulesPlugin(),
+  ],
+};
