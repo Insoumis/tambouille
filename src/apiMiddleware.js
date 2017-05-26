31bb304fa2d4d31683cc33ebc89a769d5d9fe634
Chloé Gence <chloe.gence@gmail.com>
2017-05-26 16:38:25 +0200

    remove unecessary config

--- src/apiMiddleware.js
+++ src/apiMiddleware.js
@@ -3,11 +3,8 @@ import 'firebase/database';
 
 firebase.initializeApp({
   apiKey: 'AIzaSyCL1QCLlTfJVlfnILETo2HZ5Hv8YEo3Aqg',
-  authDomain: 'tambouille-af68c.firebaseapp.com',
   databaseURL: 'https://tambouille-af68c.firebaseio.com',
   projectId: 'tambouille-af68c',
-  storageBucket: 'tambouille-af68c.appspot.com',
-  messagingSenderId: '626971325552',
 });
 const db = firebase.database();
 
