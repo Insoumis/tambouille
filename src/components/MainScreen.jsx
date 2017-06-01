b07862b911f5164fa893d7c10b8470f9e02c1bea
Baptiste <baptiste@MacBook-Air.local>
2017-06-01 23:35:31 +0200

    Integration

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import smoothscroll from 'smoothscroll';
 
 import css from './MainScreen.scss';
 
@@ -9,30 +10,41 @@ import Item from '../containers/Item';
 import { filters } from '../reducers';
 import config from '../../config';
 
-const MainScreen = () => (
-  <Router basename={config[process.env.NODE_ENV].basename}>
-    <div id="mainScreen" className={css.module}>
-      <header>
-        <h1>La tambouille c&apos;est génial (logo)</h1>
-      </header>
-      <nav>
-        {Object.keys(filters).map(cat => (
-          <NavLink
-            activeClassName={css.active}
-            key={cat}
-            to={`/categories/${cat}`}
-          >{filters[cat].name}</NavLink>
-        ))}
-      </nav>
-      <div className={css.content}>
-        <Switch>
-          <Route path="/categories/:catId" component={List} />
-          <Route path="/" component={List} />
-        </Switch>
-        <Route path="/candidats/:id" component={Item} />
-      </div>
-    </div>
-  </Router>
-);
+class MainScreen extends Component {
+
+  scrollToMainScreen() {
+    smoothscroll(document.querySelector('#mainScreen'));
+  }
+
+  render() {
+    return (
+      <Router basename={config[process.env.NODE_ENV].basename}>
+        <div className={css.module}>
+          <nav>
+            <header id="mainScreen">
+              <h2>Découvrez</h2>
+              <h2>nos tambouilles</h2>
+            </header>
+            {Object.keys(filters).map(cat => (
+              <NavLink
+                activeClassName={css.active}
+                key={cat}
+                onClick={this.scrollToMainScreen}
+                to={`/categories/${cat}`}
+              >{filters[cat].name}</NavLink>
+            ))}
+          </nav>
+          <div className={css.content}>
+            <Switch>
+              <Route path="/categories/:catId" component={List} />
+              <Route path="/" component={List} />
+            </Switch>
+            <Route path="/candidats/:id" component={Item} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default MainScreen;
