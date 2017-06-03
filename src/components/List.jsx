b5be03c9c66f157550567e994b92ae54eedb0604
Buzugu <Buzugu@users.noreply.github.com>
2017-06-03 15:18:57 +0200

    Updated titles & descriptions

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -15,7 +15,10 @@ const List = ({ category, items }) => (
     {(category) ?
       <div className={css.header}>
         <LazyImage src={macaronTalking} alt="Macaron Talking" width="230" height="228"/>
-        <h2>{filters[category].name.replace('|', ' ')}</h2>
+        <h2>{filters[category].title ?
+          filters[category].title :
+          filters[category].name.replace('|', ' ')
+        }</h2>
         <p>{filters[category].description}</p>
       </div>
     : false}
--- src/reducers.js
+++ src/reducers.js
@@ -68,37 +68,39 @@ export const getItem = (state, id) => state.items.byId[id];
 export const filters = {
   1: {
     name: 'Petits|Cadeaux',
-    description: 'Quand le PS laisse une circonscription à un copain EM ou quand EM laisse un circonscription aux copains PS et LR.',
+    title: 'Les Petits Cadeaux',
+    description: "La politique, c’est avant tout le partage et l’entraide entre camarades. C’est ainsi que le Parti Socialiste offre gracieusement des circonscriptions à ses amis de La République en marche, qui ne sont, d'ailleurs, pas en reste en proposant d’autres circonscriptions aux Républicains ou au Parti Socialiste. Après tout, en politique, l’important c’est de se serrer les coudes.",
     icon: 'ico_cadeau',
   },
   2: {
     name: 'Double|Face',
-    description: 'Les doubles investitures (candidats présents sur liste EM et PS).',
+    description: "La politique, c’est avant tout savoir s'adapter à toutes les situations, quitte à se retrouver sur deux fronts en même temps. Certains n’hésitent donc pas à se présenter aux côtés de La République en marche, tout en étant investis par le Parti Socialiste. Qu’à cela ne tienne : ils seront sermonnés par leur parti. Cependant, personne ne viendra prendre leur place et s’opposer à eux. Après tout, en politique, l’important c’est de gagner.",
     icon: 'ico_double_face',
   },
   3: {
     name: 'Alibi|Vert',
-    description: 'La politique, c’est aussi l’art des multiples alliances et des trahisons déguisées. En effet, le Parti Socialiste, ne souhaitant pas se confronter à ses anciens camarades partis chez En Marche, a décidé de sceller un accord électorale avec Europe Ecologie Les Verts, leur cédant les cirsconscriptions dans lesquelles cet affrontement avec leurs  compères auraient pu avoir lieu.',
+    description: "La politique, c’est avant tout être un bon stratège. Alliances avec les uns, trahisons avec les autres… C’est ainsi que le Parti Socialiste, soucieux de ne pas se fâcher avec ses anciens camarades désormais en marche, a préféré laisser à Europe Écologie Les Verts le soin de les affronter. Après tout, en politique, l’important c’est de participer.",
     icon: 'ico_alibi_vert',
   },
   4: {
     name: 'Place|Libre',
-    description: 'Circonscriptions où le PS a abandonné l\'investiture au profit d’EM.',
+    description: "La politique, c’est avant tout savoir quand s’arrêter. C’est ainsi que certains candidats du Parti Socialiste se sont retirés pour pouvoir soutenir des candidats de La République en Marche... Après tout, en politique, l’important c’est de défendre ses idées.",
     icon: 'ico_fauteuil',
   },
   5: {
-    name: 'Macron|Compatible',
-    description: 'Les candidats PS macron-compatibles.',
+    name: 'Macron|Compatibilité',
+    title: 'Macron-Compatibilité',
+    description: "La politique, c’est avant tout savoir se planquer. C’est ainsi que certains candidats socialistes, passés maîtres dans l’art de la dissimulation, contribuent à la confusion générale en souhaitant soutenir un gouvernement dont le premier ministre est de droite. Après tout, en politique, l’important c’est d’être bien entouré.",
     icon: 'ico_macronlover',
   },
   6: {
     name: 'Coup|De Bluff',
-    description: 'Étude statistique sur la société civile EM.',
+    description: "La politique, c’est avant tout un numéro de prestidigitation. Annoncer que l’on va présenter une majorité de candidats issus de la société civile, comme La République en marche, avant d’ajouter tous les élus MoDem. Ou encore, y placer des maires, des conseillers de cabinets ministériels… Après tout, en politique, l’important c’est de savoir faire illusion.",
     icon: 'ico_bluff',
   },
   7: {
     name: '100%|Salades',
-    description: 'Candidats EM allant à l\'encontre de l\'engagement pris de ne pas investir de personnes ayant déjà fait 3 mandats parlementaires.',
+    description: "La politique, c’est avant tout avoir le sens de l’innovation. C’est ainsi que certains candidats de La République en marche sont investis en ayant cumulé plus de trois mandats dans leur carrière, malgré les promesses données. Après tout, en politique, l’important c’est le renouvellement.",
     icon: 'ico_salade',
   },
 };
