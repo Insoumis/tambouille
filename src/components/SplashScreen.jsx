1d92ebe232b7cb572f3c7f5e0bfb6e13abcb47ab
Buzugu <Buzugu@users.noreply.github.com>
2017-06-03 16:48:24 +0200

    Redid SplashScreen

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, NavLink } from 'react-router-dom';
+
+import LazyImage from './LazyImage';
 
 import css from './SplashScreen.scss';
+import logo from './assets/tambouille_logo_x1.png';
 
 class SplashScreen extends Component {
   static propTypes = {
@@ -14,14 +17,13 @@ class SplashScreen extends Component {
   render() {
     return (
       <div className={css.module}>
-        <div className={css.image}></div>
-        <div className={css.space}></div>
-        <div className={css.wrapper}>
-          <h3 className={css.welcome}>Bienvenue!</h3>
-          <h1>Tambouille</h1>
-          <h2>électorale</h2>
-          <p className={css.tagline}>vieilles recettes politiciennes pour une nouvelle majorité</p>
-        </div>
+        <NavLink to="/">
+          <LazyImage src={logo} alt="Logo Tambouille" />
+        </NavLink>
+        <h3>Bienvenue</h3>
+        <h1>Tambouille</h1>
+        <h2>électorale</h2>
+        <p>vieilles recettes politiciennes pour une nouvelle majorité</p>
       </div>
     );
   }
