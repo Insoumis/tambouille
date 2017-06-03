fac86f776755724bfd2f93d3d0dca1b6d9fc8bb6
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2017-06-03 18:55:06 +0200

    fix: images alt

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,28 +8,32 @@ import { filters } from '../reducers';
 
 import css from './List.scss';
 
-const List = ({ category, items }) => (
-  <Container>
-    {(category) ?
-      <div className={css.header}>
-        <LazyImage src={`/assets/${filters[category].catPicture}.png`} alt="Macaron Talking" width="250" height="250"/>
-        <div className={css.content}>
-          <h2>{filters[category].title ?
-          filters[category].title :
-          filters[category].name.replace('|', ' ')}</h2>
-          <p>{filters[category].description}</p>
+const List = ({ category, items }) => {
+  const categoryTitle = filters[category].title ?
+            filters[category].title :
+            filters[category].name.replace('|', ' ')
+
+  return (
+    <Container>
+      {(category) ?
+        <div className={css.header}>
+          <LazyImage src={`/assets/${filters[category].catPicture}.png`} alt={categoryTitle} width="250" height="250"/>
+          <div className={css.content}>
+            <h2>{categoryTitle}</h2>
+            <p>{filters[category].description}</p>
+          </div>
         </div>
-      </div>
-    : false}
-    <Row>
-      {items.map(item => (
-        <Col key={item.id} md={6} lg={3}>
-          <ListItem item={item} category={category} />
-        </Col>
-      ))}
-    </Row>
-  </Container>
-);
+      : false}
+      <Row>
+        {items.map(item => (
+          <Col key={item.id} md={6} lg={3}>
+            <ListItem item={item} category={category} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
 
 List.defaultProps = {
   category: undefined,
