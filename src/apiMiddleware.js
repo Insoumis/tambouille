89a69aec7fe634ffe83abcd2fde17fddd096fb65
Buzugu <Buzugu@users.noreply.github.com>
2017-06-05 18:10:54 +0200

    Randomize candidates order

--- src/apiMiddleware.js
+++ src/apiMiddleware.js
@@ -1,5 +1,13 @@
 import data from './data.json';
 
+const randomizeOrder = (source) => {
+  const dest = {};
+  Object.keys(source).sort(() => Math.random() > 0.5).forEach((key) => {
+    dest[key] = source[key];
+  });
+  return dest;
+}
+
 export default ({ dispatch }) => next => (action) => {
   if (!action.method) {
     return next(action);
@@ -8,7 +16,7 @@ export default ({ dispatch }) => next => (action) => {
   if (action.method === 'get') {
     return dispatch({
       type: `${action.type}_SUCCESS`,
-      payload: data,
+      payload: randomizeOrder(data),
     });
   }
 
